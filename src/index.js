8da4bf6fdfe40ad0981fbc88c73be9f244eb8910
Christopher Meyer <chris@schwiiz.org>
2020-10-02 21:15:59 +0200

    refactor

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const defaultOptions = {
 	maxAttempts: 10,
-	timeout: 0
+	milliseconds: 0
 }
 
 function promiseRetry(func, options = defaultOptions, attempt = 1) {
@@ -12,7 +12,7 @@ function promiseRetry(func, options = defaultOptions, attempt = 1) {
 			if (attempt < config.maxAttempts) {
 				// return promiseRetry(func, options, attempt + 1)
 				return new Promise(resolve => {
-					setTimeout(() => resolve(promiseRetry(func, options, attempt + 1)), config.timeout)
+					setTimeout(() => resolve(promiseRetry(func, options, attempt + 1)), config.milliseconds)
 				})
 			} else {
 				throw err
